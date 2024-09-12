670593b0eb2ea1db0ff411fc569d2f1126acebb0
Onur Sahin <sahinonur2000@hotmail.com>
2024-09-12 20:27:31 +0200

    chore: update quartz

--- quartz.nix
+++ quartz.nix
@@ -8,8 +8,8 @@ let
   quartz-nix = fetchFromGitHub {
     owner = "onsah";
     repo = "quartz-nix";
-    rev = "3ef25c9717661521d3588f8f7402755db99d092e";
-    hash = "sha256-qenP3e+loTaRfIWKDDSJh+eUKddr7caQBSiZjFtUjRk=";
+    rev = "6fd3d6373f9a700ed901f2de82950e22f993bd45";
+    hash = "sha256-8VHighIus2spB1IgQiW6EiZNAhjW1YlcUj/vwEcUUVI=";
   };
 in
 callPackage "${quartz-nix}/quartz.nix" { content = config.content; }
